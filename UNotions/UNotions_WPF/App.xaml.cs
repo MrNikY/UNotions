46c1e67a56cfaf5558696fc29aa3258e2cbf9455
deus-tll <millerartemka14@gmail.com>
2023-05-22 18:24:01 +0300

    reorganizing project

--- UNotions/UNotions_WPF/App.xaml.cs
+++ UNotions/UNotions_WPF/App.xaml.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace UNotions
+namespace UNotions_WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
 }

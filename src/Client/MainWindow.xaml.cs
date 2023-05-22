d114f39776f2c9cbaab1b70212fbbdf7143815c4
Bogdan <Bogdan@DESKTOP-PGENPK7>
2023-05-23 02:17:27 +0300

    add main page

--- src/Client/MainWindow.xaml.cs
+++ src/Client/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ namespace UNotions
         public MainWindow()
         {
             InitializeComponent();
+            framePage.Content = new NotesPage();
         }
 
         #region Window buttons

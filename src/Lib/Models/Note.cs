07f60625be0fea9d85ce69b2c31ac18dacb73e24
deus-tll <millerartemka14@gmail.com>
2023-06-07 02:03:15 +0300

    changes in class Note
    implementing INotifyPropertyChanged inteface and adding field that will contain list of labels attached to certain note

--- src/Lib/Models/Note.cs
+++ src/Lib/Models/Note.cs
@@ -1,4 +1,7 @@
-﻿namespace UNotions_Lib.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace UNotions_Lib.Models
 {
 	public enum Status
 	{
@@ -7,15 +10,82 @@
 		RecycleBin
 	}
 
-	public class Note
+	public class Note : INotifyPropertyChanged
 	{
-		public int Id { get; set; }
-		public string? Title { get; set; }
-		public string? Content { get; set; }
-		public Status Status { get; set; }
-		public string? MediaContent { get; set; }
-		public DateTime CreationDate { get; set; }
-		public DateTime EditedDate { get; set; }
-		public DateTime DeletionDate { get; set; }
+		private int _id;
+		private string? _title;
+		private string? _content;
+		private string? _mediaContent;
+		private Status _status;
+		private DateTime _creationDate;
+		private DateTime _editedDate;
+		private DateTime _deletionDate;
+		private ObservableCollection<Label>? _labels;
+
+
+		public int Id
+		{
+			get { return _id; }
+			set { SetProperty(ref _id, value, nameof(Id)); }
+		}
+
+		public string? Title
+		{
+			get { return _title; }
+			set { SetProperty(ref _title, value, nameof(Title)); }
+		}
+
+		public string? Content
+		{
+			get { return _content; }
+			set { SetProperty(ref _content, value, nameof(Content)); }
+		}
+
+		public string? MediaContent
+		{
+			get { return _mediaContent; }
+			set { SetProperty(ref _mediaContent, value, nameof(MediaContent)); }
+		}
+
+		public Status Status
+		{
+			get { return _status; }
+			set { SetProperty(ref _status, value, nameof(Status)); }
+		}
+
+		public DateTime CreationDate
+		{
+			get { return _creationDate; }
+			set { SetProperty(ref _creationDate, value, nameof(CreationDate)); }
+		}
+
+		public DateTime EditedDate
+		{
+			get { return _editedDate; }
+			set { SetProperty(ref _editedDate, value, nameof(EditedDate)); }
+		}
+
+		public DateTime DeletionDate
+		{
+			get { return _deletionDate; }
+			set { SetProperty(ref _deletionDate, value, nameof(DeletionDate)); }
+		}
+
+		public ObservableCollection<Label>? Labels
+		{
+			get { return _labels; }
+			set { SetProperty(ref _labels, value, nameof(Labels)); }
+		}
+
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		private void SetProperty<T>(ref T field, T value, string name)
+		{
+			if (!EqualityComparer<T>.Default.Equals(field, value))
+			{
+				field = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+			}
+		}
 	}
 }

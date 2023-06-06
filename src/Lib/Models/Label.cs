9a1c93060f86224dc2153957f4f001bb3d64f6ba
deus-tll <millerartemka14@gmail.com>
2023-06-06 05:14:33 +0300

    adding models and enum 'Status'

---
+++ src/Lib/Models/Label.cs
@@ -0,0 +1,8 @@
+﻿namespace UNotions_Lib.Models
+{
+	public class Label
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+	}
+}
---
+++ src/Lib/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿namespace UNotions_Lib.Models
+{
+	public enum Status
+	{
+		Active,
+		Archive,
+		RecycleBin
+	}
+
+	public class Note
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Content { get; set; }
+		public Status Status { get; set; }
+		public string? MediaContent { get; set; }
+		public DateTime CreationDate { get; set; }
+		public DateTime EditedDate { get; set; }
+		public DateTime DeletionDate { get; set; }
+	}
+}
